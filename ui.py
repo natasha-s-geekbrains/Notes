5b703bb673cbaf2bda0320c51f6b0f2851254d1d
natasha_s <natasl1012@gmail.com>
2024-01-18 23:09:48 +0300

    added def start_menu

--- ui.py
+++ ui.py
@@ -1,3 +1,16 @@
 def start_menu():
-    return 1
+    command = None
+    while command !=6:
+        command = int(input("Здравствуйте! Вы находитесь в приложении по управлению заметками!\n"
+                            "Выберите, что вы хотите сделать:\n"
+                            "1. Создать заметку\n"
+                            "2. Вывести все заметки\n"
+                            "3. Редактировать заметку\n"
+                            "4. Выйти из приложения\n"
+                            "Введите номер выбранной команды: "))
+        comand=check_command_num(command)
 
+    return n
+
+def check_command_num(n):
+    return n
