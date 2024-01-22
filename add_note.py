c3e5c784b02eb16d75f354b1b48a6cd10b94616a
natasha_s <natasl1012@gmail.com>
2024-01-23 00:56:46 +0300

    added find_date method and other improvements

--- add_note.py
+++ add_note.py
@@ -4,7 +4,7 @@ import datetime
 def add_note():
     note_title = input("Введите заголовок заметки и нажмите клавишу Enter: ")
     note_text = input("Введите текст заметки и нажмите клавишу Enter: ")
-    storage_date_time = datetime.datetime.now()
+    storage_date_time = str(datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
     note_ID = get_next_note_ID()
 
     with open("notes_file.txt", "a", encoding="utf-8") as file:
