b3b65cc426d5dcda3357394cb49c443c914981f4
natasha_s <natasl1012@gmail.com>
2024-01-22 22:32:48 +0300

    added change of date format

--- add_note.py
+++ add_note.py
@@ -4,14 +4,13 @@ import datetime
 def add_note():
     note_title = input("Введите заголовок заметки и нажмите клавишу Enter: ")
     note_text = input("Введите текст заметки и нажмите клавишу Enter: ")
-    storage_date_time = str(datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
+    storage_date_time = datetime.datetime.now()
     note_ID = get_next_note_ID()
 
     with open("notes_file.txt", "a", encoding="utf-8") as file:
         file.write(f"{note_ID};{note_title};{note_text};{storage_date_time}\n")
 
     print("Заметка успешно сохранена!\n")
-
 
 
 def get_next_note_ID():
